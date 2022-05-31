de985585deaeffdc2ef5a2cac48357da4e3944fa
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-05-31 00:46:28 -0400

    Time: 163 ms (5.03%), Space: 13.9 MB (44.00%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,38 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # for elemantary addition
+        head = ListNode()
+        current = head
         carry = 0
-        
-        temp = ListNode()
-        curr = temp
-        
-        while l1 or l2 or carry:
-            
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-            # carry
-            val  =  v1 + v2 + carry
-            carry = val // 10
-            val = val % 10
-            curr.next = ListNode(val)
-            
-            
-            # updating the pointers
-            curr = curr.next
-            # move the pointers in l1 and l2
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            
-        return temp.next
-            
-            
-            
-            
-            
-            
-            
-            
-        
+        while l1 is not None or l2 is not None:
+            result = carry
+            if l1 is not None:
+                result += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                result += l2.val
+                l2 = l2.next
+            current.next = ListNode(result%10)
+            if result > 9:
+                carry = 1
+            else:
+                carry = 0
+            current = current.next
+		#adds the overflow carry to end of the ouutput linked list
+        if carry == 1:
+            current.next = ListNode(carry)
+        return head.next
