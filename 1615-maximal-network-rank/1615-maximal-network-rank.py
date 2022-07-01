37dd965ebb2b9ee0095c0cfde48082f7a8f3300d
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-06-30 22:06:49 -0400

    Time: 658 ms (21.76%), Space: 15.6 MB (70.33%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -3,16 +3,19 @@ class Solution:
         
         graph = defaultdict(list)
         
+        # bulidng an adjacency graph list
+        
         for u,v in roads:
             graph[u].append(v)
             graph[v].append(u)
             
         res = 0
-        
+    
         for i in range(n):
             for j in range(i + 1, n):
+                # calcualting the road length
                 roadLen = len(graph[i]) + len(graph[j])
-                
+                # removing road connected to both cities
                 if j in graph[i]:
                     roadLen -= 1
                     
