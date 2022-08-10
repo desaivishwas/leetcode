abf478889a3d24cd2f3b0a3787d5727444a2bc08
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-08-09 21:10:09 -0400

    Time: 5962 ms (74.44%), Space: 14 MB (9.83%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,22 +1,32 @@
 class Solution:
-    maxGoldFound = 0 
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                self.dfs(grid, i, j, 0)
-        return self.maxGoldFound
-
-    def dfs(self, grid, i, j, currGold):
-        width, height = len(grid[0]), len(grid)
-        if i < 0 or j < 0 or i >= height or j >= width or grid[i][j] == 0:
-            return 0
-        goldInCell = grid[i][j]
-        totalGold = currGold + goldInCell
-        if totalGold > self.maxGoldFound:
-            self.maxGoldFound = totalGold
-        grid[i][j] = 0
-        self.dfs(grid, i - 1, j, totalGold)
-        self.dfs(grid, i + 1, j, totalGold)
-        self.dfs(grid, i, j + 1, totalGold)
-        self.dfs(grid, i, j - 1, totalGold)
-        grid[i][j] = goldInCell
+        # Time: O(MN)
+        # Space: O(MN)
+        row, col = len(grid), len(grid[0])
+        
+        
+        def dfs(i,j):
+            currGold  = grid[i][j]
+            grid[i][j] = 0
+            goldFound = 0
+            for x,y in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                if 0 <= x < row and 0 <= y < col and grid[x][y] != 0:
+                    goldFound = max(goldFound, dfs(x,y))
+            grid[i][j] = currGold
+            
+            return goldFound + currGold
+        
+        
+        
+        goldFound = 0
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] != 0:
+                    goldFound = max(goldFound, dfs(r, c))
+                
+        return goldFound
+    
+    
+        
+            
+    
