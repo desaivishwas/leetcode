b41b5d3952c2ce0d6c0992522b5fb81a653f907f
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-08-15 16:08:28 -0400

    Time: 58 ms (80.71%), Space: 14.4 MB (67.19%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # dict have iverheads hence array since the data is smaller
+        # hash tavles take extra space
+        # codepath 06/07
+        
+        # Time: O(n)
+        # Space: O(1)
+        
+        
+        if len(s) != len(t):
+            return False
+            
+        char_counts = [0] * 26
+        
+        for i in range(len(s)):
+            char_counts[ord(s[i]) - ord('a')] += 1
+            char_counts[ord(t[i]) - ord('a')] -= 1
+                
+        for value in char_counts:
+            if value != 0:
+                return False
+        return True
+        
