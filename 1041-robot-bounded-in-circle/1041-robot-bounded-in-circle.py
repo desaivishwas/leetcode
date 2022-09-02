bfd77b2782309b9027bc6942c1ea62827d0fff44
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-01 21:18:35 -0400

    Time: 60 ms (17.50%), Space: 13.9 MB (71.58%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -1,6 +1,7 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         # directions
+        # putting robot in north direction
         dirX, dirY = 0, 1
         x, y = 0, 0
         
@@ -8,8 +9,10 @@ class Solution:
             if dir == "G":
                 x, y = x + dirX, y + dirY
             elif dir == "L":
+                # anti clock wise direction
                 dirX, dirY = -1 * dirY, dirX
             else:
+                # turns right
                 dirX, dirY = dirY, -1 * dirX
                 
         return (x, y) == (0,0) or (dirX, dirY) != (0,1)
