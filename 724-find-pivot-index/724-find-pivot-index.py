0822c7d85c2c29538bee5c26f944b84415f2757b
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-02 10:45:16 -0400

    Time: 350 ms (12.53%), Space: 15.1 MB (92.28%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -3,6 +3,7 @@ class Solution:
         total = sum(nums)
         lsum = 0
         for i in range(len(nums)):
+            # calculate right sum wich toal - curr - leftsum
             rsum = total - nums[i] - lsum
             if lsum == rsum:
                 return i 
