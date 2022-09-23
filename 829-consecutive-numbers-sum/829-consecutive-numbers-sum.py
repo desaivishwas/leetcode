91f265841d2949b520da5bcbaddef6940073d5a3
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-23 15:08:16 -0400

    Time: 77 ms (96.23%), Space: 13.8 MB (53.58%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
@@ -1,11 +1,26 @@
 class Solution:
     def consecutiveNumbersSum(self, n: int) -> int:
-        count, k = 0, 1
-        while 2 * n > k * (k-1):
-            numerator = n - (k * (k-1) // 2)
-            if numerator % k == 0:
-                count += 1
+        ''' Ciount the oddd factors to get the sol'''
+        
+        count = 0
+        for i in range(1, int(math.sqrt(n)) + 1):
+            if  n % i == 0:
+                if i % 2:
+                    count += 1
+                    
+                if (n/i) % 2 and n/i != i:
+                    count += 1
+                    
+                    
+        return count
+        
+        
+#         count, k = 0, 1
+#         while 2 * n > k * (k-1):
+#             numerator = n - (k * (k-1) // 2)
+#             if numerator % k == 0:
+#                 count += 1
             
-            k += 1
-        return count 
+#             k += 1
+#         return count 
         
