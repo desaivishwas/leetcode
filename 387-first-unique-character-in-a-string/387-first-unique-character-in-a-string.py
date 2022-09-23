f708a4027f5d56c8317788066d21023f15692508
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-23 15:00:18 -0400

    Time: 189 ms (45.00%), Space: 14.2 MB (16.61%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -10,16 +10,3 @@ class Solution:
                 return index
         return -1
             
-        
-        
-#         for c in s:
-#             if c in char:
-#                 char[c] += 1
-#             else:
-#                 char[c] = 1
-                
-                
-#         for i, ch in enumerate(s):
-#             if char[ch] == 1:
-#                 return i
-#         return -1
