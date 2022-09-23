8a4334a7407a3868a037a3ff444bc917b26f4e7b
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-23 15:13:40 -0400

    Time: 207 ms (72.07%), Space: 13.8 MB (96.73%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
@@ -15,6 +15,25 @@ class Solution:
         return count
         
         
+        '''The reason why the number of "odd factors" turns out to be the answer is because:
+            Say the sum of x, x+1, x+2, ..., x+k is N, the sum can be rewritten in
+            (x + (x+k)) * (k + 1) / 2 = N, and we are looking for all integer pairs x and k that can satisfy this equation.
+            The left hand side can be rearranged to
+            ((2x + k) / 2) * (k + 1) = N or (2x + k) * (k + 1) / 2 = N which we can further simplify to
+            (x + k/2) * (k + 1) = N or (2x + k) * ((k + 1) / 2) = N. This is where the magic happens:
+
+            Observe that
+
+            when k is an even number, k+1 will be an odd number. And because (x + k/2) * (k + 1) = N we know that (x + k/2) * odd = N.
+            when k is an odd number, 2x+k will be an odd number. And because (2x + k) * ((k + 1) / 2) = N we know that odd * ((k + 1) / 2) = N.
+            What this means is that the left hand of the equation will always have an odd multiplier. So far we have proved that for every x and k pair that we are looking for, one of the multiplier on the left hand side is an odd factor of N. Now we need to prove that for every odd factor of N, we can have a integer solution for x and k:
+
+            when k is an even number and k+1 is the odd factor of N. Then (x + k/2) * odd = N and x + k/2 = N / odd factor. Since k is even x + k/2 will be an integer and therefore solvable.
+            when k is an odd number and 2x+k is the odd factor of N. Then odd* ((k + 1) / 2) = N and (k + 1) / 2 = N / odd factor. Since k is odd k + 1 will be even and (k + 1) / 2 will be an integer and therefore solvable.
+            Now we have proven that the number of x and k pairs such that (x + (x+k)) * (k + 1) / 2 = N is the exact number of N’s odd factors.
+'''
+        
+        
 #         count, k = 0, 1
 #         while 2 * n > k * (k-1):
 #             numerator = n - (k * (k-1) // 2)
