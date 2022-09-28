b310f923d998e7909faaf1b7412fe43839a2d363
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-09-28 15:44:08 -0400

    Time: 64 ms (19.51%), Space: 13.9 MB (62.91%) - LeetHub

---
+++ 965-univalued-binary-tree/965-univalued-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        n = set()
+        
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                n.add(node.val)
+
+                if node.left:
+                    stack.append(node.left)
+                    
+                if node.right:
+                    stack.append(node.right)
+                    
+                    
+        return len(n) == 1
