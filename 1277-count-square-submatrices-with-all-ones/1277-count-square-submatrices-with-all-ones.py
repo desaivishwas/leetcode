e9f987e8bb30300d52ddb47910c95d3e11ff6453
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-10-09 21:48:22 -0400

    Time: 652 ms (90.08%), Space: 16.3 MB (70.96%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,12 +1,15 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
+        
+        ''' O(MN)  O(m * n)'''
+        
         num_rows = len(matrix)
         num_cols = 0 if num_rows < 0 else len(matrix[0])
         # Add an extra all zero column and row outside of the actual dp table
         # to simplify the transition
         
         dp = [[0]*(num_cols+1) for _ in range(num_rows+1)]
-        
+         
         num_squares = 0
         
         for i in range(1, num_rows + 1):
