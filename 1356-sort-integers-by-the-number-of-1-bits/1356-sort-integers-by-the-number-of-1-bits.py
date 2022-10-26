98073b18a9fcc90d12deacff25b41e8c180204a2
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-10-25 20:45:37 -0400

    Time: 124 ms (67.51%), Space: 14.1 MB (78.02%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,26 +1,21 @@
 class Solution:
-    def helper(self, n):
-        binary = ""
-        if (n != 0):
-            while (n >= 1):
-                if (n % 2 == 0):
-                    binary = binary + "0"
-                    n = n // 2
-                else:
-                    binary = binary + "1"
-                    n = (n-1) // 2
-
-        return "".join(reversed(binary))
-
     def sortByBits(self, arr: List[int]) -> List[int]:
-        arr.sort()
-        d = []
-        for num in arr:
-            d.append((num, self.helper(num).count('1')))
+        def helper(n, res = ""):
+            if n == 0:
+                return (res)
+            else:
+                res = str(n % 2) + res
+                n = n // 2
+                return helper(n, res)
             
-        d = sorted(d, key=lambda x:x[1])
+        res = []
+        arr.sort()
+        for num in arr:
+            res.append((num, helper(num, "").count("1")))
+            
+        res.sort(key = lambda x:x[1])
         
-        return [x[0] for x in d]
-                        
+        return [k for (k,val) in res]
+            
         
         
