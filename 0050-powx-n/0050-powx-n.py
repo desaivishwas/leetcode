ec918c2a76127f7012fa1f5b7df70700a57b1ec5
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-11-02 23:00:53 -0400

    Time: 71 ms (8.20%), Space: 13.9 MB (20.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        # return x ** n
+        if n == 0:
+            return 1
+        elif n < 0:
+            x = 1 / x
+            n = -n
+            
+        power = x
+        multiplications = 1
+        while n - multiplications * 2 >= 0:
+            power *= power
+            multiplications *= 2
+
+        return power * self.myPow(x, n - multiplications)
