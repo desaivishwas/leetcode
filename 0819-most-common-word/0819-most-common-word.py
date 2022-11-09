2b7ca56ee0a2b8ef27d94d977fb407cc869c51c7
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-11-08 22:19:24 -0500

    Time: 88 ms (13.47%), Space: 13.9 MB (82.07%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -2,11 +2,9 @@ class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         # if len(paragraph) == 1 and len(banned) == 0:
         #     return paragraph
-        normalized_str = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
         d = {}
-        words = normalized_str.split()
-        # des = [word.lower().strip("!?',;.") for word in words]
-        
+        normalized_str = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+        words = normalized_str.split()        
         for w in words:
             if w not in banned:
                 d[w] = 1 + d.get(w, 0)
