131dbd54c76a81fe40df9ee6fe27374c0bef1d69
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-11-16 17:49:49 -0500

    Time: 69 ms (36.08%), Space: 14 MB (18.13%) - LeetHub

--- 0760-find-anagram-mappings/0760-find-anagram-mappings.py
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.py
@@ -1,7 +1,7 @@
 class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
         d = defaultdict(list)  
-        for num in nums1:
-            d[num].append(nums2.index(num))
+        for i, num in enumerate(nums2):
+            d[num].append(i)
             
         return [d[num].pop() for num in nums1]
