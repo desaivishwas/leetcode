30901edb81386c09d778ec0f280cff3b19eda347
Vishwas Desai <43722290+desaivishwas@users.noreply.github.com>
2022-11-28 22:03:25 -0500

    Time: 800 ms (69.85%), Space: 13.9 MB (96.63%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-        memo = [0]*len(s)
-        
+        memo = [0] * len(s)
         temp, prev = 0, 0
+        
         for i in range(len(s)-2, -1, -1):
             prev = 0
             for j in range(i+1, len(s)):
@@ -12,4 +12,5 @@ class Solution:
                 else:
                     memo[j] = 1 + min(memo[j], memo[j-1])
                 prev = temp
-        return memo[len(s)-1] <= k
+                
+        return memo[len(s) - 1] <= k
